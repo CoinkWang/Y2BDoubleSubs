ecd465605ea8a2fd8ad682e2493e371c75831abe
CoinkWang <coinkwang@qq.com>
2020-04-16 20:09:29 +0800

    update 1.6.1 修复无法正确加载评论的问题

--- hook_request.js
+++ hook_request.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Youtube double language subtitle / Youtube 双语字幕 
-// @version      1.5
+// @version      1.6.1
 // @description  Youtube double language subtitle / Youtube 双语字幕. 如果不能自动加载，请关闭字幕再次打开即可。默认语言为浏览器首选语言。
 // @author       Coink
 // @match        *://www.youtube.com/watch?v=*
 // @match        *://www.youtube.com
 // @match        *://www.youtube.com/*
-// @require      https://unpkg.com/ajax-hook@2.0.0/dist/ajaxhook.min.js
+// @require      https://unpkg.com/ajax-hook@2.0.2/dist/ajaxhook.min.js
 // @grant        none
 // @namespace    https://github.com/CoinkWang/Y2BDoubleSubs
 // ==/UserScript==
@@ -15,6 +15,9 @@
     let localeLang = navigator.language ? navigator.language : 'en'
     // localeLang = 'zh'  // uncomment this line to define the language you wish here
     ah.proxy({
+        onRequest: (config, handler) => {
+            handler.next(config);
+        },
         onResponse: (response, handler) => {
             if (response.config.url.includes('/api/timedtext') && !response.config.url.includes('&translate_h00ked')){
                 let xhr = new XMLHttpRequest();
@@ -31,7 +34,10 @@
                 if (defaultJson.events.length === localeJson.events.length) {
                     // when length of segments are the same
                     for (let i = 0,len = defaultJson.events.length; i<len; i++) {
-                        defaultJson.events[i].segs[0].utf8 += ('\n' + localeJson.events[i].segs[0].utf8)
+                        if (defaultJson.events[i].segs[0].utf8 !== localeJson.events[i].segs[0].utf8){
+                            // not merge subs while the are the same
+                            defaultJson.events[i].segs[0].utf8 += ('\n' + localeJson.events[i].segs[0].utf8)
+                        }
                     }
                     response.response =  JSON.stringify(defaultJson)
                 } else {
